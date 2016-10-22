a4579e2c371e2ce25eaca4efa948fbc8e004c978
qq519043202 <519043202@qq.com>
2016-10-22 21:02:21 +0800

    首页 merge

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -11,7 +11,7 @@ return array(
 	// 'DB_PREFIX' => 'think_', // 数据库表前缀 
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
-	'SHOW_PAGE_TRACE' => TRUE, 
+	'SHOW_PAGE_TRACE' => TRUE, // 数据库调试信息
 	'TMPL_PARSE_STRING'  =>array(
      '__JS__'     => __ROOT__.'/Public/js', // 增加新的JS类库路径替换规则
      '__CSS__'     => __ROOT__.'/Public/css', // 增加新的CSS类库路径替换规则
