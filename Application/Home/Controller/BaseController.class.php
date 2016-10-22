712a42a7048e17c59a3df705bcdf2507d47dce23
hitlxc <1127225095@qq.com>
2016-10-23 00:27:07 +0800

    地图模块

--- Application/Home/Controller/BaseController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class BaseController extends Controller {
-    public function _empty(){
-		$this->redirect('Index/index');
-    }
-}
--- Application/Home/Controller/EmptyController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class EmptyController extends BaseController{
-    public function index(){
-        $this->redirect('Index/index');
-    }
-}
