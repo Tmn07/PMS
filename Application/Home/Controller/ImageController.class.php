9d45b1661e7b7e1c01cee1aae23ee1f86340681e
tmn07 <519043202@qq.com>
2016-11-18 16:31:47 +0800

    美化界面demo

--- Application/Home/Controller/ImageController.class.php
+++ Application/Home/Controller/ImageController.class.php
@@ -2,10 +2,16 @@
 namespace Home\Controller;
 use Think\Controller;
 class ImageController extends BaseController {
-    public function api(){
+
+    public function index()
+    {
+        $this->display();
+    }
+
+    public function api($n1,$n2,$n3){
         // $this->show('imageapi');
         dump(getcwd());
-        $cmd = system("/usr/bin/python /opt/lampp/htdocs/PMS/py/api.py t.png -i 11 51 51",$ret);
+        $cmd = system("/usr/bin/python /opt/lampp/htdocs/PMS/py/api.py t.png -i $n1 $n2 $n3",$ret);
     	echo("ret is $ret");
         // $ret = shell_exec("/usr/bin/python /opt/lampp/htdocs/PMS/py/api.py t.png -i 11 51 51");
         // dump($ret);
